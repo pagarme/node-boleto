5377eeae445b7dc20314bd14308fd076694eea5b
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2016-04-09 03:17:37 -0300

    Commit santander

--- banks/bradesco/helper.js
+++ banks/bradesco/helper.js
@@ -4,4 +4,4 @@ exports.dateFromEdiDate = (ediDate) => {
 	return new Date(parseInt("20" + ediDate.substring(4, 8)),
 		parseInt(ediDate.substring(2, 4)) - 1,
 		parseInt(ediDate.substring(0, 2)));
-}
+};
